ac319100d631981cb18f2896d1f0b6801cb26304
Junhwan <jhwan.kim333@gmail.com>
2019-06-10 02:52:48 +0900

    Sudoku Solver

---
+++ Sudoku_Solver.cpp
@@ -0,0 +1,277 @@
+#include "Sudoku.h"
+#include "Sudoku_Solver.h"
+
+using namespace std;
+
+/*Look at Sudoku_Solver.h for documention on member functions' constraints and (side-)effects*/
+
+Sudoku_Solver::Sudoku_Solver(istream &is)
+	: sudoku(is) {
+	size = sudoku.get_size();
+	tracker.resize(size);
+}
+
+
+bool Sudoku_Solver::solve_helper(std::unordered_set<unsigned short> &cumulative_conflict_set, size_t depth) {
+	//find block with smallest domain
+	unsigned short row = (unsigned short) size; //temporary place holder
+	unsigned short col = (unsigned short) size; //temporary place holder
+	unsigned short min_domain_size = (unsigned short) (size+1); //temporary place holder
+	for (unsigned short i = 0; i < size; ++i) {
+		auto domain_size = tracker[i].second;
+		if (domain_size == 0) {
+			//found conflict
+			row = i;
+			col = tracker[i].first;
+
+			auto &cs = sudoku.get_conflict_set(row, col);
+			//cumulative_conflict_set.clear(); //DO NOT clear conflict_set, all conflicts matter!
+			cumulative_conflict_set.insert(cs.begin(), cs.end());
+			return false;
+
+		} else if (domain_size < min_domain_size) {
+			row = i;
+			col = tracker[i].first;
+			min_domain_size = domain_size;
+		}
+	}
+
+	unsigned short key = sudoku.get_key(row, col);
+
+	//assign value and continue search
+	auto &domain = sudoku.get_domain(row, col);
+	for (auto it = domain.begin(); it != domain.end(); ++it) {
+		set_val_and_update(row, col, *it);
+
+		//stop search when sudoku board is full (entirely through legel moves)
+		if (sudoku.get_num_blank() == 0) {
+			return true;
+		}
+
+		//if conflict was found in the next iteration
+		//or if next iteration could not solve conflict
+		if (solve_helper(cumulative_conflict_set, depth+1) == false) {
+			unset_val_and_update(row, col); //undo
+			//if current block is in cumulative conflict set
+			if (cumulative_conflict_set.find(key) != cumulative_conflict_set.end()) {
+				continue; //continue search with next value
+			} else {
+				return false; //move back up a depth
+			}
+		} else {
+			return true; //propagate return true from when board is full
+		}
+	}
+
+	if (depth == 0) {
+		//i.e. current block is initial block the search started with
+		throw Sudoku_Error(); //exhausted search space
+	} else {
+		//current block was in cumulative_conflict_set,
+		//but all its values led to a conflict
+		cumulative_conflict_set.erase(key);
+		auto &cs = sudoku.get_conflict_set(row, col);
+		cumulative_conflict_set.insert(cs.begin(), cs.end());
+		return false;
+	}
+}
+
+
+bool Sudoku_Solver::solve() {
+	if (sudoku.is_solved()) {
+		return true;
+	} else if (sudoku.get_num_blank() == 0) {
+		throw Sudoku_Error();
+	}
+
+	pre_solve();
+
+	if (sudoku.is_solved()) {
+		return true;
+	}
+
+	//depth first search that uses forward checking and conflict-directed backjumping
+	unordered_set <unsigned short> cumulative_conflict_set;
+	solve_helper(cumulative_conflict_set);
+
+	return sudoku.is_solved();
+}
+
+
+void Sudoku_Solver::track_row(unsigned short row) {
+	unsigned short min_col;
+	size_t min_domain_size = size+1; //temporary place holder
+	for (unsigned short col = 0; col < size; ++col) {
+		if (sudoku.get_val(row, col) != Block::BLANK) {
+			continue;
+		}
+		auto new_size = sudoku.get_domain(row,col).size();
+		if (new_size < min_domain_size) {
+			min_col = col;
+			min_domain_size = new_size;
+		}
+	}
+	tracker[row] = make_pair(min_col, min_domain_size);
+}
+
+void Sudoku_Solver::pre_solve() {
+	//update all domain
+	sudoku.update_all_domains();
+
+	//start keeping track of blocks with minimum remaing values (min domain)
+	for (unsigned short i = 0; i < size; ++i) {
+		track_row(i);
+	}
+
+	unsigned short next = 0;
+	while (next != size) {
+		//find next block to fill (i.e. block with domain size 1)
+		for (next = 0; next < size; ++next) {
+			auto domain_size = tracker[next].second;
+			if (domain_size == 0) {
+				throw Sudoku_Error();
+			} else if (domain_size == 1) {
+				unsigned short row = next;
+				unsigned short col = tracker[next].first;
+				auto &cur_domain = sudoku.get_domain(row, col);
+				auto val_it = cur_domain.begin();
+				set_val_and_update(row, col, *val_it); //set_val, update domains and tracker
+				break;
+			}
+		}
+	}
+}
+
+void Sudoku_Solver::set_val_and_update(unsigned short row, unsigned short col, unsigned short val) {
+	sudoku.set_val(row, col, val);
+
+	//remove from domains in same col
+	//add its key to conflict_sets in same col
+	for (unsigned short i = 0; i < size; ++i) {
+		if (sudoku.get_val(i, col) != Block::BLANK) {
+			continue;
+		}
+		auto tracker_val = tracker[i].second;
+		if (sudoku.domain_erase(i, col, val)) {
+			sudoku.conflict_set_insert(i,col,row,col);
+		}
+		auto new_size = sudoku.get_domain_size(i, col);
+		//update tracking (note domain size changes by max 1)
+		if (new_size <= tracker_val) {
+			//faster pruning using operator<= by moving to a related block next (maybe?)
+			tracker[i] = make_pair(col, new_size);
+		}
+	}
+
+	//remove from domains in same row
+	//add its key to conflict_sets in same row
+	for (unsigned short j = 0; j < size; ++j) {
+		if (sudoku.get_val(row, j) != Block::BLANK) {
+			continue;
+		}
+		if (sudoku.domain_erase(row, j, val)) {
+			sudoku.conflict_set_insert(row,j,row,col);
+		}
+	}
+
+	//remove in same square
+	//add its key to conflict_sets in same square
+	auto small_size = sudoku.get_small_size();
+	unsigned short start_row = (unsigned short) (row - row%small_size);
+	unsigned short start_col = (unsigned short) (col - col%small_size);
+	for (unsigned short i = start_row; i < start_row + small_size; ++i) {
+		for (unsigned short j = start_col; j < start_col + small_size; ++j) {
+			if (i == row) {
+				continue;
+			} else if (j == col) {
+				continue;
+			} else if (sudoku.get_val(i, j) != Block::BLANK) {
+				continue;
+			}
+			auto tracker_val = tracker[i].second;
+			if (sudoku.domain_erase(i, j, val)) {
+				sudoku.conflict_set_insert(i,j,row,col);
+			}
+			auto new_size = sudoku.get_domain_size(i,j);
+			//update tracking (note domain size changes by max 1)
+			if (new_size <= tracker_val) {
+				//faster pruning using operator<= by moving to a related block next (maybe?)
+				tracker[i] = make_pair(j, new_size);
+			}
+		}
+	}
+
+	//update tracking for the row which the block val was set
+	track_row(row);
+}
+
+void Sudoku_Solver::unset_val_and_update(unsigned short row, unsigned short col) {
+	unsigned short val = sudoku.get_val(row, col);
+
+	//need re-tracking only if the block modified is the one in tracker
+	unordered_set<unsigned short> need_track;
+
+	//add to domains in same col
+	//delete its key to conflict_sets in same col
+	for (unsigned short i = 0; i < size; ++i) {
+		if (sudoku.get_val(i, col) != Block::BLANK) {
+			continue;
+		}
+		if (tracker[i].first == col) {
+			need_track.insert(i);
+		}
+		if (sudoku.conflict_set_erase(i, col, row, col)) {
+			sudoku.domain_insert(i, col, val);
+		}
+	}
+
+	//add to domains in same row
+	//delete its key to conflict_sets in same row
+	for (unsigned short j = 0; j < size; ++j) {
+		if (sudoku.get_val(row, j) != Block::BLANK) {
+			continue;
+		}
+		if (sudoku.conflict_set_erase(row, j, row, col)) {
+			sudoku.domain_insert(row, j, val);
+		}
+	}
+	need_track.insert(row); //whole row was modified, definitely need re-tracking
+
+	//add to domains in same square
+	//delete its key to conflict_sets in same square
+	auto small_size = sudoku.get_small_size();
+	unsigned short start_row = (unsigned short) (row - row%small_size);
+	unsigned short start_col = (unsigned short) (col - col%small_size);
+	for (unsigned short i = start_row; i < start_row + small_size; ++i) {
+		for (unsigned short j = start_col; j < start_col + small_size; ++j) {
+			if (sudoku.get_val(i, j) != Block::BLANK) {
+				continue;
+			}
+			if (tracker[i].first == j) {
+				need_track.insert(i);
+			}
+			if (sudoku.conflict_set_erase(i, j, row, col)) {
+				sudoku.domain_insert(i, j, val);
+			}
+		}
+	}
+
+	//unset value here so we don't triple check current block
+	sudoku.set_val(row, col, Block::BLANK); //unset val to BLANK
+
+	//update tracking
+	for (auto it = need_track.begin(); it != need_track.end(); ++it) {
+		track_row(*it);
+	}
+}
+
+
+void Sudoku_Solver::print(ostream &os) const {
+	sudoku.print_board(os);
+	os << "\n";
+}
+
+
+bool Sudoku_Solver::is_solved() const {
+	return sudoku.is_solved();
+}
---
+++ Sudoku_Solver.h
@@ -0,0 +1,86 @@
+#ifndef SUDOKU_SOLVER_H
+#define SUDOKU_SOLVER_H
+
+#include <iostream>
+#include <vector>
+#include <utility> //pair, make_pair()
+#include <string>
+#include <unordered_set>
+
+#include "Sudoku.h"
+
+//A sudoku solver that uses depth first search and
+//applys 'forward checking' and 'conflict-direct backjumping'
+//to solve any n by n sudoku
+class Sudoku_Solver {
+public:
+	//REQUIRES: istream argument satisfies the requirement for Sudoku class constructor
+	//MODIFIES: sudoku, size
+	//EFFECTS: create a Sudoku_Solver object
+	Sudoku_Solver(std::istream &is);
+
+	//MODIFIES: sudoku, tracker
+	//EFFECTS: attempts to solve sudoku, throws Sudoku_Error() if unsolvable
+	bool solve();
+
+	//EFFECTS: prints sudoku board to ostream
+	void print(std::ostream &os) const;
+
+	//EFFECTS: returns true iff sudoku is solved
+	bool is_solved() const;
+
+private:
+	//MODIFIES: sudoku, tracker
+	//EFFECTS: updates domains of all empty blocks, then solves sudoku only to 
+	//		the point all values are 100% certain
+	// 		i.e. fill in sudoku until none of the blocks have domain size of 1
+	void pre_solve();
+
+	Sudoku sudoku;
+
+	//for each row, tracks blocks with minimum remaining-values (i.e. min domain size)
+	//using pair(col, domain size)
+	std::vector<std::pair<unsigned short, unsigned short> > tracker;
+
+	unsigned short size;
+
+	//REQUIRES: row, col are smaller than size
+	//			val is non-negative and smaller or equal to size
+	//MODIFIES: tracker, value of sudoku block at (row,col),
+	//			domain and conflict_sets of sudoku blocks in same row, same col, same sqaure
+	//EFFECTS: sets value of sudoku block at (row,col) as val
+	//			removes val from domains of blank blocks in the same row, same col, same sqaure
+	//			adds key of the block at (row,col) to the conflict_set of block which had their domain reduced
+	void set_val_and_update(unsigned short row, unsigned short col, unsigned short val);
+
+	//REQUIRES: row, col are smaller than size
+	//			sudoku block at (row,col) is not blank
+	//MODIFIES: tracker, value of sudoku block at (row,col),
+	//			domain and conflict_sets of sudoku blocks in same row, same col, same sqaure
+	//EFFECTS: sets value of sudoku block at (row,col) back to blank.
+	//			adds val of the block at (row,col) to domains of blank blocks in the same row,
+	// 			same col, same sqaure only if the key of the block at (row,col) is in their conflict_set
+	void unset_val_and_update(unsigned short row, unsigned short col);
+
+	//REQUIRES: row is smaller than size
+	//MODIFIES: tracker
+	//EFFECTS: updates tracker at index row to contain pair(col, domain_size)
+	//		whereby col indicates the index of the block at row that has the minimum domain size
+	void track_row(unsigned short row);
+
+	//REQUIRES: cumulative_conflict_set is empty
+	//MODIFIES: cumulative_conflict_set, sudoku, tracker
+	//EFFECTS: recursively calls itself to solve the sudoku using
+	// 		depth first search that uses forward tracking and conflict-directed back jumping
+	bool solve_helper(std::unordered_set<unsigned short>&cumulative_conflict_set, size_t depth = 0);
+};
+
+
+//Expection thrown when Sudoku has no solution
+class Sudoku_Error {
+public:
+	std::string msg = "Sudoku has no solution.";
+};
+
+
+#endif
